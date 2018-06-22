5589e55d54c892e21d0f3cf88be65727290f966a
ema0312 <39930409+ema0312@users.noreply.github.com>
2018-06-22 00:21:12 -0500

    Add files via upload

---
+++ Father.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace homework15
+{
+    class Father    
+    {
+        public string _nickName { get; set; }
+        public int _age { get; set; }
+
+        public Father(string name, int age)
+        {
+            _nickName = name;
+            _age = age;
+        }
+    }
+}
